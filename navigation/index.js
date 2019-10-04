d60f4e2f341f0797bb1d6b5f51ab0f2350573e3c
bmannan1337 <basim_mannan@hotmail.com>
2019-10-04 17:25:29 +0500

    Final Commit

--- navigation/index.js
+++ navigation/index.js
@@ -4,30 +4,46 @@ import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 
 import Welcome from "../screens/Welcome";
-// import Login from "../screens/Login";
-// import Browse from "../screens/Browse";
-// import Explore from "../screens/Explore";
-// import Product from "../screens/Product";
-// import Settings from "../screens/Settings";
+import Login from "../screens/Login";
+import SignUp from "../screens/SignUp";
+import Forgot from "../screens/Forgot";
+import Browse from "../screens/Browse";
+import Explore from "../screens/Explore";
+import Product from "../screens/Product";
+import Settings from "../screens/Settings";
 
 import { theme } from "../constants";
 
 const screens = createStackNavigator(
   {
-    Welcome
-    // Login,
-    // Browse,
-    // Explore,
-    // Product,
-    // Settings
+    Welcome,
+    Login,
+    SignUp,
+    Forgot,
+    Browse,
+    Explore,
+    Product,
+    Settings
   },
   {
     defaultNavigationOptions: {
-      headerStyle: {},
-      headerBackImage: <Image />,
+      headerStyle: {
+        height: theme.sizes.base * 4,
+        backgroundColor: theme.colors.white,
+        borderBottomColor: "transparent",
+        elevation: 0 // for android
+      },
+      headerBackImage: <Image source={require("../assets/icons/back.png")} />,
       headerBackTitle: null,
-      headerLeftContainterStyle: {},
-      headerRightContainerStyle: {}
+      headerLeftContainterStyle: {
+        alignItems: "center",
+        marginLeft: theme.sizes.base * 2,
+        paddingRight: theme.sizes.base
+      },
+      headerRightContainerStyle: {
+        alignItems: "center",
+        paddingRight: theme.sizes.base
+      }
     }
   }
 );
